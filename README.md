f88c2db9d253fc1fe02e1dd43af720d17bc31a7b
Stujo <stujo@github.com>
2015-08-27 14:21:48 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Link 4
+
+An experiment with sockets to create a multiplayer game
+
---
+++ dist/bundle.js
@@ -0,0 +1,19 @@
+// Link4 App JavaScript
+
+function Link4App(socket){
+
+}
+
+var app = new Link4App(io());
+
+// $('form#chatter').submit(function(){
+//   socket.emit('chat message', $('#m').val());
+//   $('#m').val('');
+//   return false;
+// });
+
+// socket.on('chat message', function(msg){
+//   $('#messages').append($('<li>').text(msg));
+// });
+
+
