15863381df014e30dcd49d2e1d11164ffb7b2de2
Stujo <stujo@github.com>
2015-08-28 18:32:54 -0700

    Add simple Cell CSS

--- src/javascripts/game/view.js
+++ src/javascripts/game/view.js
@@ -8,12 +8,21 @@ export default class Link4View {
   render(model) {
     this._debugDiv().innerHTML = model.serialize();
 
+    var board = this.boardDiv();
+
+    var current_player = board.querySelector('.current_player');
+
+    current_player.innerHTML = model.currentTurn();
+
     for(var col = 0; col < model.COLUMN_COUNT; col ++){
-      var column = this.boardDiv().querySelector('.board_column[data-column="' + col + '"]');
+      var column = board.querySelector('.board_column[data-column="' + col + '"]');
       for(var row = 0; row < model.ROW_COUNT; row ++){
          var state = model.at(col,row); 
          var cell = column.querySelector('.cell[data-row="' + row + '"]');
-         cell.innerHTML = state;
+         if(state != '_'){
+           this._addClass(this._stateToCellClass(state), cell);
+         }
+         cell.innerHTML = this._stateToCellContents(state);
       }
     }
   }
@@ -26,6 +35,34 @@ export default class Link4View {
     var dropCallback =  this._dropCallback.bind(this, callbacks.drop);
     var grid = this.boardDiv().querySelector('.grid');
     grid.addEventListener('click', dropCallback);
+  }
+
+  _stateToCellContents(state){
+     if (state === '_') { return '&nbsp;' }
+     return state;
+  }
+  _stateToCellClass(state){
+     return 'player_' + state;
+  }
+
+  _addClass( classname, element ) {
+    var cn = element.className;
+    //test for existance
+    if( cn.indexOf( classname ) != -1 ) {
+      return;
+    }
+    //add a space if the element already has class
+    if( cn != '' ) {
+      classname = ' '+classname;
+    }
+    element.className = cn+classname;
+  }
+
+  _removeClass( classname, element ) {
+      var cn = element.className;
+      var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
+      cn = cn.replace( rxp, '' );
+      element.className = cn;
   }
 
   _dropCallback(cb, e){
